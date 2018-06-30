acedea487d45746d872a23ef2dc65ff2e2247c6c
g0g05arui <mihai.indreias@gmail.com>
2018-06-30 18:59:30 +0300

    Update vector.h

--- vector.h
+++ vector.h
@@ -1,7 +1,7 @@
 #ifndef VECT_H_INCLUDED
 #define VECT_H_INCLUDED
 #include <fstream>
-
+#include <typeinfo>
 template<class T>
 class Vector{
 	private:
@@ -20,9 +20,6 @@ class Vector{
 		void pop_back();///Erases the last element of the vector
 		void pop_front();///Erases the first element
 		void clear();///Clears the vectors
-		typedef T * iterator;///Iterator
-		iterator begin();///First position
-		iterator end();///Last position
 		T &back();///Last element
 		T &front();///First element
 		Vector<T> operator + (Vector<T> aux);///Returns the current elemnts with the elements of aux in the back
@@ -31,6 +28,21 @@ class Vector{
 		Vector<T> operator + (int x);///Applies +=x;
 		Vector<T> operator / (int x);///Applies /=x;
 		Vector<T> operator % (int x);///Applies %=x;
+        class iterator{
+            private:
+                T *x;///Pointer to the current value
+            public:
+                const std::string TypeName=typeid(T*).name();///Name of the pointer's class
+                T & operator *();///Returns the value of the pointer
+                void operator ++ ();///Goes to the next pointer
+                void operator ++ (int);///Postfix
+                void operator =(const iterator &);///....(self-exp)
+                bool operator !=(const iterator &);///....(self-exp)
+                iterator(const T*);///constructor to a pointer
+                iterator();///NULL constructor
+        };
+        iterator begin();///First position
+		iterator end();///Last position
 };
 template <class T>
 void operator <<(std::ofstream & out,Vector<T> v);///Outputs the vector v to the ofstream out;
